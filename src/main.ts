918f09b92a82ac3d8c462771748400815033ff35
Vladislav Sergeev <v.sergeev@raison.finance>
2026-02-03 22:14:05 +0300

    build: Configure Tailwind CSS v4 and Pinia

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
-console.log('Init');
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import './style.css'
+import App from './App.vue'
+
+const app = createApp(App)
+app.use(createPinia())
+app.mount('#app')
